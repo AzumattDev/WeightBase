62db71134047882a0940fc8c1c05f1c3c7b0da7b
AzumattDev <azumattgaming@gmail.com>
2024-05-16 00:23:57 -0400

    v1.1.6, Ashlands recompile

--- Plugin.cs
+++ Plugin.cs
@@ -20,7 +20,7 @@ namespace WeightBase;
 public class WeightBasePlugin : BaseUnityPlugin
 {
     internal const string ModName = "WeightBase";
-    internal const string ModVersion = "1.1.5";
+    internal const string ModVersion = "1.1.6";
     internal const string Author = "MadBuffoon";
     private const string ModGUID = Author + "." + ModName;
     private const string ConfigFileName = ModGUID + ".cfg";
